12e25a8d879c83da159a6c08f690aac1e496bf9f
Tatiana2433 <8833@holbertonstudents.com>
2024-02-23 10:11:45 +0000

    Ask 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - afficher l'alphabet
+ * Return: zero
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
